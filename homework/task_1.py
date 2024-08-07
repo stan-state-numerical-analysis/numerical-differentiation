1da7661bf5146bb551385b25add639edf0818e0d
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-07 14:38:00 -0700

    Update task_1.py

--- homework/task_1.py
+++ homework/task_1.py
@@ -5,27 +5,17 @@
 
 
 
-# 2. Define a function called example_function that takes a number x as input and outputs the value of the function
-# f(x) = 5x^5-3x^3+2x+1.
-
-
-
-# 3. Define a function called true_second_derivative that takes a number x as input and outputs the value of the true second derivative
-# of the example_function given above.
-
-
-
-# 4. Define a function called plot_second_derivative_approximation that takes a function f, its true second derivative function f_dd,
+# 2. Define a function called plot_second_derivative_approximation that takes a function f, its true second derivative function f_dd,
 # a point x, and a list of h values. The function should plot the estimated second derivative using the central difference method
 # and the true second derivative at x for the given h values. The variable names and plot settings should be identical to those used in class,
 # besides the plot label, axhline label, ylabel, and title. Be sure to import the necessary package.
 
 
 
-# 5. Define a function called calculate_absolute_error that takes an estimated value and the true value, and returns the absolute error.
+# 3. Define a function called calculate_absolute_error that takes an estimated value and the true value, and returns the absolute error.
 
 
 
-# 6. Define a function called plot_absolute_error that takes a function f, its true second derivative function f_dd, a point x,
+# 4. Define a function called plot_absolute_error that takes a function f, its true second derivative function f_dd, a point x,
 # and a list of h values. The function should plot the absolute error of the estimated second derivative at x for the given h values.
 # The variable names and plot settings should be identical to those used in class, besides the title.
