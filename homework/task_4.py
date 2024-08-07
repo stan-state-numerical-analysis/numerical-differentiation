6b8c278e99378ebb8df60ef02eb3770109bee1af
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-07 15:19:30 -0700

    Create task_4.py

---
+++ homework/task_4.py
@@ -0,0 +1,18 @@
+# Task: Use the functions defined in task_3 to analyze the first derivative using the central difference method for a given function on an interval.
+
+# 1. Import the task_3 module.
+
+
+
+# 2. Define a function called example_function that takes a number x as input and outputs the value of the function
+# f(x) = 5x^5-3x^3+2x+1.
+
+
+
+# 3. Define a function called true_derivative that takes a number x as input and outputs the value of the true first derivative
+# of the example_function given above.
+
+
+
+# 4. Use the plot_derivative_approximations function from the task_3 module to visualize how well the central difference method
+# approximates the first derivative of the example_function on the interval [-2, 2] with h-values: 2, 1, 0.5, 0.1, 0.01.
