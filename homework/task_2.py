26c63672c14dfd9b67e6edc305436fab5ca7b597
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-07 14:45:50 -0700

    Update task_2.py

--- homework/task_2.py
+++ homework/task_2.py
@@ -1,1 +1,23 @@
+# Task: Use the functions defined in task_1 to analyze the second derivative using the central difference method for a given function.
 
+# 1. Import the task_1 module.
+
+
+
+# 2. Define a function called example_function that takes a number x as input and outputs the value of the function
+# f(x) = 5x^5-3x^3+2x+1.
+
+
+
+# 3. Define a function called true_second_derivative that takes a number x as input and outputs the value of the true second derivative
+# of the example_function given above.
+
+
+
+# 4. Use the plot_second_derivative_approximation function from the task_1 module to visualize how well the central difference method
+# approximates the second derivative of the example_function at the x-value 4 with h-values: 1, 0.5, 0.25,...,2^(-10).
+
+
+
+# 5. Use the plot_absolute_difference function from the task_1 module to visualize the absolute error of the central
+# difference method in approximating the second derivative of the example_function at the x-value 4 with h-values: 1, 0.5, 0.25,...,2^(-10).
