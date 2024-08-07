d67694244a34778725edc146779f537c6d30d1a9
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-07 15:15:06 -0700

    Create task_3.py

---
+++ homework/task_3.py
@@ -0,0 +1,38 @@
+# Task: Create functions that approximate the first derivative using the central difference method, and visualize the accuracy for various step sizes on a given interval.
+
+# 1. Import the necessary plotting package.
+
+
+
+# 2. Define a function called central_difference that takes a function f, a point x, and a small value h as input,
+# and outputs the approximate first derivative of f at x using the central difference method.
+
+
+
+# 3. The function below takes in the original function, its true derivative, an interval of x-values in the form of a 2-element list, and list of h-values.
+# It will plot the true first derivative on the interval as well as the approximations for different h-values.
+# Finish the function definition below by completing the three TO DOs.
+
+def plot_derivative_approximations(func, true_derivative, interval, h_values):
+    
+    # TODO: Define a list called true_derivatives that contains the true derivative at each value in the interval.
+    
+
+    plt.figure(figsize=(14, 8))
+  
+    # TODO: Plot the true derivative in the color black with the label True Derivative.
+
+  
+
+    # TODO: For each h-value, define a list called approx_derivatives that approximates the true
+    # derivative using the central difference method (with the given h-value) at each x-value in
+    # the interval. Then plot the approximate derivative and give it the label Approx Derivative (h=#)
+    # where # should be replaced with the corresponding value of h.
+
+
+    plt.xlabel('x')
+    plt.ylabel('Derivative')
+    plt.title('First Derivative Approximation Using Central Difference Method')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
